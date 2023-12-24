693e0c5a4af58443c6ab8af919c1d4bd322eabce
zian <mynameiszian@gmail.com>
2023-12-25 01:34:44 +0800

    feat: complete implementation of rooms

--- room/room.go
+++ room/room.go
@@ -1,18 +1,21 @@
 package room
 
 import (
+	"fmt"
+
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
 
 type User struct {
 	Id         uuid.UUID
-	connection websocket.Conn
+	connection *websocket.Conn
 }
 
 type Room struct {
-	Users map[uuid.UUID]*User
-	Id    uuid.UUID
+	Users    map[uuid.UUID]*User
+	Id       uuid.UUID
+	chatPipe chan string
 }
 
 var rooms map[uuid.UUID]*Room
@@ -21,44 +24,96 @@ var users map[uuid.UUID]*User
 type UserActions func(map[uuid.UUID]*User)
 type RoomActions func(map[uuid.UUID]*User)
 
-func GenerateId() uuid.UUID {
+func generateId() uuid.UUID {
 	return uuid.New()
+}
+
+func CreateRoom() *Room {
+	room := new(Room)
+	room.Id = generateId()
+	room.Users = make(map[uuid.UUID]*User)
+	room.chatPipe = make(chan string)
+
+	rooms[room.Id] = room
+
+	sendChat(room)
+
+	return room
+}
+
+func createRoomWithId(id uuid.UUID) *Room {
+	room := CreateRoom()
+	room.Id = id
+
+	return room
+}
+
+func sendChat(room *Room) {
+	go func() {
+		for {
+			message := <-room.chatPipe
+			for _, user := range room.Users {
+				user.connection.WriteMessage(1, []byte(message))
+			}
+		}
+	}()
+
+}
+
+func AddUser(conn *websocket.Conn, roomId uuid.UUID) *User {
+	user := new(User)
+	user.Id = generateId()
+	user.connection = conn
+
+	users[user.Id] = user
+
+	room, exist := rooms[roomId]
+
+	if !exist {
+		room = createRoomWithId(roomId)
+	}
+
+	room.Users[user.Id] = user
+
+	listenChatFromUser(user, room)
+
+	return user
+}
+
+func listenChatFromUser(user *User, room *Room) {
+	go func() {
+		for {
+			msgType, msg, err := user.connection.ReadMessage()
+			message := string(msg)
+			message = user.Id.String() + ": " + message
+
+			if websocket.IsCloseError(err) {
+				removeUser(user, room)
+				return
+			}
+
+			if err != nil {
+				fmt.Println("Error reading message: ", err)
+				continue
+			}
+
+			if msgType != websocket.TextMessage {
+				fmt.Println("Not a text message")
+				continue
+			}
+
+			room.chatPipe <- message
+		}
+	}()
+
+}
+
+func removeUser(user *User, room *Room) {
+	delete(room.Users, user.Id)
+	delete(users, user.Id)
 }
 
 func InitRooms() {
 	rooms = make(map[uuid.UUID]*Room)
 	users = make(map[uuid.UUID]*User)
-}
-
-func DoUsersInRoom(callback UserActions, roomId uuid.UUID) {
-	users := GetUsersInRoom(roomId)
-	callback(users)
-}
-
-func CreateRoom() *Room {
-	room := new(Room)
-	room.Id = GenerateId()
-	rooms[room.Id] = room
-
-	return room
-}
-
-func GetRoom(id uuid.UUID) *Room {
-	return rooms[id]
-}
-
-func GetUsersInRoom(id uuid.UUID) map[uuid.UUID]*User {
-	return users
-}
-
-func CreateUser(conn websocket.Conn) *User {
-	user := new(User)
-	user.Id = GenerateId()
-	user.connection = conn
-	users[user.Id] = user
-
-	return user
-}
-
-func AddUserToRoom(roomId uuid.UUID, user *User) {
 }
